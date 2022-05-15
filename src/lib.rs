24586fd189f7e50bdccf5c0f8492b9f13a9f44b0
Douglas Oliveira <douglas4030@gmail.com>
2022-05-15 12:48:24 -0300

    feat add sha2

--- src/lib.rs
+++ src/lib.rs
@@ -40,4 +40,16 @@ mod tests {
         bloom.push(&42);
         assert!(bloom.contains(&42));
     }
+
+    #[test]
+    fn sha2() {
+        use sha2::Digest;
+
+        let mut hash = sha2::Sha512::new();
+        hash.update("123");
+        let hashed = hash.finalize();
+        let str_hash = hex::encode(hashed);
+
+        assert_eq!(str_hash, "3c9909afec25354d551dae21590bb26e38d53f2173b8d3dc3eee4c047e7ab1c1eb8b85103e3be7ba613b31bb5c9c36214dc9f14a42fd7a2fdb84856bca5c44c2")
+    }
 }
